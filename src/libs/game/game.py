998d2f32700d4a12fb416879f786ee168398bdb3
d0gied <german.n.u@mail.ru>
2024-07-12 22:26:06 +0300

    update

--- src/libs/game/game.py
+++ src/libs/game/game.py
@@ -1,7 +1,8 @@
 from config import Config
-from ..models.core import CommandPayload
+from ..models.core import CommandPayload, CommandSuccess, CommandError, CommandResponse
 from typing import Any
-
+from requests import post, get, put, Session
+import urllib3
 
 class Game:
   def __init__(
@@ -9,9 +10,13 @@ class Game:
     *,
     api_base_url: str = Config.Server.API_BASE_URL,
   ) -> None:
-    self.api_base_url = api_base_url
+    self.api_base_url = api_base_url.strip("/") + "/"
 
-  def _command(self, payload: CommandPayload) -> None: ...
+  def _command(self, payload: CommandPayload) -> CommandResponse:
+    response = post(self.api_base_url + "zombidef/command", json=payload.model_dump())
+    if response.status_code != 200:
+      return CommandError.model_validate(response.json())
+    return CommandSuccess.model_validate(response.json())
 
   def _participate(self) -> None: ...
 
--- src/libs/models/core.py
+++ src/libs/models/core.py
@@ -16,6 +16,12 @@ class CommandPayload(BaseModel):
   build: list[BuildCommand] = Field(..., examples=[{"x": 1, "y": 1}])
   move_base: Coordinate = Field(..., alias="moveBase", examples=[{"x": 1, "y": 1}])
 
-class CommandResponse(BaseModel):
+class CommandResponse(BaseModel): ...
+
+class CommandError(CommandResponse):
+  err_code: int = Field(..., alias="errCode", examples=[22])
+  error: str = Field(..., examples=["description of the error"])
+
+class CommandSuccess(CommandResponse):
   accepted_commands: CommandPayload = Field(..., alias="acceptedCommands", examples=[{"attack": [{"blockId": "f47ac10b-58cc-0372-8562-0e02b2c3d479", "target": {"x": 1, "y": 1}}], "build": [{"x": 1, "y": 1}], "moveBase": {"x": 1, "y": 1}}])
   errors: list[str] = Field(..., examples=["coordinate at {0 0} is already occupied"])
