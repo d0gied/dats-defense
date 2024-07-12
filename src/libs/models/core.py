3588138678102f6534be34416ad030a58663ec6b
d0gied <german.n.u@mail.ru>
2024-07-12 21:56:16 +0300

    update core models

--- src/libs/models/core.py
+++ src/libs/models/core.py
@@ -8,5 +8,10 @@ class AttackCommand(BaseModel):
   block_id: str = Field(..., alias="blockId", examples=["f47ac10b-58cc-0372-8562-0e02b2c3d479"])
   target: Coordinate = Field(..., examples=[{"x": 1, "y": 1}])
 
-class CommandPayload:
+class BuildCommand(Coordinate):
+  pass
+
+class CommandPayload(BaseModel):
   attack: list[AttackCommand] = Field(..., examples=[{"blockId": "f47ac10b-58cc-0372-8562-0e02b2c3d479", "target": {"x": 1, "y": 1}}])
+  build: list[BuildCommand] = Field(..., examples=[{"x": 1, "y": 1}])
+  move_base: Coordinate = Field(..., alias="moveBase", examples=[{"x": 1, "y": 1}])
